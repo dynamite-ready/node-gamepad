9d556e33419bbb026df6c266b7532e226a3f93f8
dynamite-ready <l_r_charles@hotmail.com>
2014-07-20 19:27:47 +0100

    Update index.js
    Node Gamepad 20/07/2014: Adding explicit XBox360 and PS3 joypad support. Dpad 'buttons' are interpreted as axes, and the Xbox360 analogue paddles trigger button events.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 var nodeGamepad = require("./build/Release/nodeGamepad");
 var e = require("events");
 
+// Separate file for this, most likely.
+var joypadConfig = { 
+	ps3: {
+		dpad: [
+			// Button numbers.
+			{ number: 4, action: -1, axis: "yaxis" },
+			{ number: 5, action: 1, axis: "xaxis" },
+			{ number: 6, action: 1, axis: "yaxis" },
+			{ number: 7, action: -1, axis: "xaxis" }
+		]
+	},
+	
+	xbox360: {
+		dpad: [
+			// Button numbers.
+			{ number: 0, action: -1, axis: "yaxis" },
+			{ number: 1, action: 1, axis: "yaxis" },
+			{ number: 2, action: -1, axis: "xaxis" },
+			{ number: 3, action: 1, axis: "xaxis" }
+		],
+		
+		triggers: [ 4, 5 ] // Axes... AKA analogue buttons.
+	}
+};
+
 // Constructor...
 function Device(deviceID, deadzone){
 	var _self = this;
 	this.events = new e.EventEmitter();
 	this.deviceID = deviceID;
+	this.dirValueSet = [];
+	
 	try {	
 		if(nodeGamepad.deviceCount >= Number(this.deviceID + 1))
 			this.device = new nodeGamepad.Gamepad(this.deviceID || 0);
@@ -38,42 +65,115 @@ Device.prototype = {
 	},
 	
 	pollDevice: function(callback){
+		this.dirValueSet = []
 		if(this.device && nodeGamepad.deviceCount >= Number(this.deviceID + 1)){
 			this.pollButtons(this.device.getButtonStateArray());
 			this.pollAxes(this.device.getAxisStateArray());
 			if(callback) callback();
+			// console.log(this.device.getButtonStateArray())
+			// console.log(this.device.getAxisStateArray());
 		}
 	},
 	
 	pollButtons: function(stateArray){
+		var _self = this;
+		
 		if(this.device && nodeGamepad.deviceCount >= Number(this.deviceID + 1)){
 			for(var button in stateArray){
 				if(stateArray[button]){
-					this.events.emit("buttondown", Number(button + 1));
+					switch(this.name){
+						case "PLAYSTATION(R)3 Controller":
+							dpad(joypadConfig.ps3.dpad, button);
+							break;
+						case "XInput Controller #1":
+							dpad(joypadConfig.xbox360.dpad, button);
+							break;
+						default:
+							this.events.emit("buttondown", Number(button + 1));
+							break;
+					}
 				};
 			};	
 		}
+		
+		function dpad(map, button){
+			var isDpad = false;
+			for(var dbutton in map){
+				if(button == map[dbutton]["number"]){
+					isDpad = true;
+					// Adequate, but this needs to be cleaner.
+					if(map[dbutton]["axis"] == "yaxis"){
+						if(!_self.dirValueSet[1])
+							_self.events.emit("yaxis", map[dbutton]["action"]);
+						
+						_self.dirValueSet[1] = true;
+					} else {
+						if(!_self.dirValueSet[0]) 
+							_self.events.emit("xaxis", map[dbutton]["action"]);
+
+						_self.dirValueSet[0] = true;
+					}
+				}
+			};
+			
+			if(!isDpad)
+				_self.events.emit("buttondown", Number(button + 1));			
+		}	
 	},
 	
 	pollAxes: function(stateArray){
+		var _self = this;
 		// Any even number (0, 2, 4) will be assumed to address the X axis,
 		// and odd numbers, Y in this implementation. Dicking about with the successive axes
 		// will cancel out the preceding pair.
+		
 		if(this.device && nodeGamepad.deviceCount >= Number(this.deviceID + 1)){
-			for(var axis in stateArray){
+			for(var axis = 0; axis < stateArray.length; axis++){
 				var axisValue = 0;
-				if(stateArray[axis] != 0){
+				if(stateArray[axis] != 0){	
 					// Calculate the joystick deadzone...
 					if(this.deadzone){
-						if(stateArray[axis] < Number(this.deadzone) && stateArray[axis] > -Number(this.deadzone))
-							stateArray[axis] = 0;
+						var evaluateDeadzone = (
+							stateArray[axis] < Number(this.deadzone) && 
+							stateArray[axis] > -Number(this.deadzone)
+						);
 					}
-					if(stateArray[axis] > 0) axisValue = 1;
-					if(stateArray[axis] < 0) axisValue = -1;
+						
+					if(!evaluateDeadzone){				
+						if(stateArray[axis] > 0) axisValue = 1;
+						if(stateArray[axis] < 0) axisValue = -1;
+					}
 					
-					axis == 0 || (axis % 2) == 0 ?
-						this.events.emit("xaxis", Number(axisValue)):
-						this.events.emit("yaxis", Number(axisValue));
+					if(this.name == "XInput Controller #1"){
+						if(
+							axis == joypadConfig.xbox360.triggers[0] ||
+							axis == joypadConfig.xbox360.triggers[1]
+						){
+							if(axisValue >= 1){
+								if(axis == joypadConfig.xbox360.triggers[0]){
+									this.events.emit("buttondown", "L");
+								}
+								
+								if(axis == joypadConfig.xbox360.triggers[1]){
+									this.events.emit("buttondown", "R");
+								}
+							} 
+						} else emitAxisEvent();
+					} else emitAxisEvent();
+					
+					function emitAxisEvent(){
+						if(Number(axis % 2) == 0){
+							if(!_self.dirValueSet[0] && Number(axisValue)) 
+								_self.events.emit("xaxis", Number(axisValue));
+							if(Number(axisValue)) _self.dirValueSet[0] = true;
+							//console.log("XAXIS: ", axisValue);
+						} else {
+							if(!_self.dirValueSet[1] && Number(axisValue)) 
+								_self.events.emit("yaxis", Number(axisValue));
+							if(Number(axisValue)) _self.dirValueSet[1] = true;
+							//console.log("YAXIS: ", axisValue);
+						}
+					}
 				}
 			}
 		}
@@ -86,23 +186,10 @@ Device.prototype = {
 			nodeGamepad.deviceCount == 0
 		);
 		
-		if(this.device) if(this.device) this.device.close();
+		if(this.device) this.device.close();
 		if(deviceShouldClose) delete this;
 		if(callback) callback();
 	}
 };
-
-/*
-global.setInterval(function(){
-	// Check for signs of life.
-	_self.gamepad.checkDevice();
-}, 1500);
-var gamepad = new Device(0);
-global.setInterval(function(){
-	// Read the pad, render the frame.
-	gamepad.checkDevice();
-}, 17);		
-*/
-
 
 module.exports = Device;
